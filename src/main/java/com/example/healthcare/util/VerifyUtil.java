9f97d71b10dbacf8f231c0d2aa0f077c1dec5672
hyeonjun <zlel175@naver.com>
2024-06-15 19:54:42 +0900

    regex, verity util 추가

---
+++ src/main/java/com/example/healthcare/util/VerifyUtil.java
@@ -0,0 +1,11 @@
+package com.example.healthcare.util;
+
+public class VerifyUtil {
+
+  public static final int MIN_DEFAULT_LENGTH = 1;
+  public static final int MAX_DEFAULT_LENGTH = 20;
+
+  public static final int MAX_EMAIL_LENGTH = 254;
+  public static final int MAX_MOBILE_LENGTH = 1;
+
+}
