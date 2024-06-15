acc6d36c65128aa90663bf62dbcbc871df39dd79
K-IMjihun <kjh950901@gmail.com>
2024-06-15 17:44:40 +0900

    feat: 회원가입 스톡 api

--- src/main/java/com/example/healthcare/account/controller/AnUserController.java
+++ src/main/java/com/example/healthcare/account/controller/AnUserController.java
@@ -1,4 +1,24 @@
 package com.example.healthcare.account.controller;
 
+import com.example.healthcare.account.service.UserAnService;
+import com.example.healthcare.account.service.dto.SignUpDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/")
 public class AnUserController {
+  private final UserAnService userAnService;
+
+  // 회원가입
+  @PostMapping("an/users/sign-up")
+  public void signUp(@RequestBody SignUpDTO signUpDTO){
+    userAnService.signUp(signUpDTO);
+  }
+
 }
--- src/main/java/com/example/healthcare/account/service/UserAnService.java
+++ src/main/java/com/example/healthcare/account/service/UserAnService.java
@@ -1,4 +1,20 @@
 package com.example.healthcare.account.service;
 
+import com.example.healthcare.account.repository.UserRepository;
+import com.example.healthcare.account.service.dto.SignUpDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
 public class UserAnService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  public void signUp(SignUpDTO signUpDTO) {
+
+  }
 }
