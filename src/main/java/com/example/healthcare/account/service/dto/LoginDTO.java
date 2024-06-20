a7bc59550919866ac597475020427cfcaae6b248
K-IMjihun <kjh950901@gmail.com>
2024-06-20 13:25:35 +0900

    LoginDTO

---
+++ src/main/java/com/example/healthcare/account/service/dto/LoginDTO.java
@@ -0,0 +1,20 @@
+package com.example.healthcare.account.service.dto;
+
+import com.example.healthcare.util.RegexUtil;
+import com.example.healthcare.util.VerifyUtil;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class LoginDTO {
+  @NotBlank
+  @Length(min = VerifyUtil.MIN_DEFAULT_LENGTH, max = VerifyUtil.MAX_DEFAULT_LENGTH)
+  @Pattern(regexp = RegexUtil.REGEXP_LOGIN_ID, message = "invalid login id")
+  String loginId;
+  @NotBlank
+  @Length(min = 8, max = VerifyUtil.MAX_DEFAULT_LENGTH)
+  @Pattern(regexp = RegexUtil.REGEXP_PASSWORD, message = "invalid password format")
+  String password;
+}
