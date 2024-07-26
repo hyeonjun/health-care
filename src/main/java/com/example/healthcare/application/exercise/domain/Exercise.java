b57a0bc1c559c38cc14b59b59f99250ba9b87243
hyeonjun <zlel175@naver.com>
2024-07-26 23:29:18 +0900

    운동 종목 테이블: index 추가, 생성 user 외래키 추가

--- src/main/java/com/example/healthcare/application/exercise/domain/Exercise.java
+++ src/main/java/com/example/healthcare/application/exercise/domain/Exercise.java
@@ -1,19 +1,45 @@
 package com.example.healthcare.application.exercise.domain;
 
+import com.example.healthcare.application.account.domain.User;
+import com.example.healthcare.application.common.domain.Base;
+import com.example.healthcare.application.exercise.controller.dto.CreateExerciseDTO;
 import com.example.healthcare.application.exercise.domain.code.ExerciseBodyType;
 import com.example.healthcare.application.exercise.domain.code.ExerciseToolType;
-import com.example.healthcare.application.exercise.controller.dto.CreateSyExerciseDTO;
-import com.example.healthcare.application.common.domain.Base;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Objects;
 
 @Entity
 @DynamicUpdate
 @Getter
+@Builder(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+  name = "exercise",
+  indexes = {
+    @Index(columnList = "exercise_body_type, user_id")
+  }
+)
 public class Exercise extends Base {
 
   @Id
@@ -27,6 +53,8 @@ public class Exercise extends Base {
   @Column(name = "description", length = 3000)
   private String description;
 
+  private boolean isDeleted;
+
   @Enumerated(value = EnumType.STRING)
   @Column(name = "exercise_body_type", nullable = false, length = 191)
   private ExerciseBodyType bodyType;
@@ -35,11 +63,45 @@ public class Exercise extends Base {
   @Column(name = "exercise_tool_type", nullable = false, length = 191)
   private ExerciseToolType toolType;
 
-  public Exercise(CreateSyExerciseDTO dto){
-   this.name = dto.name();
-   this.bodyType = dto.bodyType();
-   this.toolType = dto.toolType();
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "user_id")
+  private User createdUser;
+
+  public static Exercise createExercise(CreateExerciseDTO dto, User user) {
+    return Exercise.builder()
+      .name(dto.name())
+      .description(dto.description())
+      .isDeleted(Boolean.FALSE)
+      .bodyType(dto.bodyType())
+      .toolType(dto.toolType())
+      .createdUser(user)
+      .build();
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Exercise exercise)) return false;
+    return Objects.equals(getId(), exercise.getId());
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getId());
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+      .append("id", id)
+      .append("name", name)
+      .append("description", description)
+      .append("isDeleted", isDeleted)
+      .append("bodyType", bodyType)
+      .append("toolType", toolType)
+      .append("createdUser", createdUser != null ? createdUser.getId() : null)
+      .append("createdDateTime", createdDateTime)
+      .append("updatedDateTime", updatedDateTime)
+      .toString();
+  }
 }
