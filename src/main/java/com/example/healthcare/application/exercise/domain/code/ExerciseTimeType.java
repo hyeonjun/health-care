0b2a1207d49dc1bcd0841cec130e43abb095e3e4
hyeonjun <zlel175@naver.com>
2024-07-26 23:30:04 +0900

    exerciseTime description

--- src/main/java/com/example/healthcare/application/exercise/domain/code/ExerciseTimeType.java
+++ src/main/java/com/example/healthcare/application/exercise/domain/code/ExerciseTimeType.java
@@ -1,7 +1,17 @@
 package com.example.healthcare.application.exercise.domain.code;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ExerciseTimeType {
 
-  DAY, EVENING, NIGHT
+  DAY("07:00 ~ 15:00"),
+  EVENING("15:00 ~ 23:00"),
+  NIGHT("23:00 ~ 07:00")
+  ;
+
+  private final String description;
 
 }
