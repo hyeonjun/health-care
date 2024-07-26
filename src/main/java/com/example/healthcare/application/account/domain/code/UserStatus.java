53ecd2613bdedbcee34ffa40f4f2b135a8d0248c
hyeonjun <zlel175@naver.com>
2024-07-26 17:05:44 +0900

    userStatus 탈퇴 명칭 수정, 중지된 상태 관련 Set 필드 추가

--- src/main/java/com/example/healthcare/application/account/domain/code/UserStatus.java
+++ src/main/java/com/example/healthcare/application/account/domain/code/UserStatus.java
@@ -2,17 +2,25 @@ package com.example.healthcare.application.account.domain.code;
 
 import lombok.Getter;
 
+import java.util.Set;
+
 @Getter
 public enum UserStatus {
 
   ACTIVATED("활성화"),
   DEACTIVATED("비활성화"),
-  SECESSION("탈퇴"),
+  CANCELLED("탈퇴"),
   ;
 
   private final String description;
 
   UserStatus(String description) {
     this.description = description;
+  }
+
+  private static final Set<UserStatus> SUSPENDED_STATUSES = Set.of(DEACTIVATED, CANCELLED);
+
+  public static Set<UserStatus> getSuspendedStatuses() {
+    return SUSPENDED_STATUSES;
   }
 }
