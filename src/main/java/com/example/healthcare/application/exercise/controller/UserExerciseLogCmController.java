b7de6c31d746124c6217aec461c01e188d1e3879
hyeonjun <zlel175@naver.com>
2024-07-27 18:37:45 +0900

    userExerciseLog controller 클래스명 변경

--- src/main/java/com/example/healthcare/application/exercise/controller/UserExerciseLogCmController.java
+++ src/main/java/com/example/healthcare/application/exercise/controller/UserExerciseLogCmController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/cm/exercise-logs")
-public class UserExerciseLogController {
+public class UserExerciseLogCmController {
 
   private final UserExerciseLogCmService userExerciseLogCmService;
 
