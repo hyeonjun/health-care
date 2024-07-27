973aaf0cd9634f74162a0132dbbecd96c14b47c4
hyeonjun <zlel175@naver.com>
2024-07-27 23:45:17 +0900

    create user exercise log, routine, set api

--- src/main/java/com/example/healthcare/application/exercise/helper/ExerciseHelper.java
+++ src/main/java/com/example/healthcare/application/exercise/helper/ExerciseHelper.java
@@ -3,14 +3,23 @@ package com.example.healthcare.application.exercise.helper;
 import com.example.healthcare.application.account.domain.User;
 import com.example.healthcare.application.common.exception.AuthException;
 import com.example.healthcare.application.common.exception.AuthException.AuthExceptionCode;
+import com.example.healthcare.application.common.exception.InvalidInputValueException;
+import com.example.healthcare.application.common.exception.InvalidInputValueException.InvalidInputValueExceptionCode;
+import com.example.healthcare.application.common.exception.ResourceException;
+import com.example.healthcare.application.common.exception.ResourceException.ResourceExceptionCode;
 import com.example.healthcare.application.exercise.controller.dto.CreateExerciseDTO;
+import com.example.healthcare.application.exercise.controller.dto.CreateUserExerciseRoutineDTO;
 import com.example.healthcare.application.exercise.domain.Exercise;
 import com.example.healthcare.application.exercise.domain.ExerciseTypeRelation;
+import com.example.healthcare.application.exercise.domain.UserExerciseRoutine;
+import com.example.healthcare.application.exercise.domain.UserExerciseSet;
 import com.example.healthcare.application.exercise.domain.code.ExerciseType;
 import com.example.healthcare.application.exercise.exception.ExerciseException;
 import com.example.healthcare.application.exercise.exception.ExerciseException.ExerciseExceptionCode;
 import com.example.healthcare.application.exercise.repository.ExerciseRepository;
 import com.example.healthcare.application.exercise.repository.ExerciseTypeRelationRepository;
+import com.example.healthcare.application.exercise.service.data.UserExerciseData.UserExerciseLogData;
+import com.example.healthcare.application.exercise.service.data.UserExerciseData.UserExerciseRoutineData;
 import com.example.healthcare.application.vo.ExerciseDetailVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -53,6 +62,37 @@ public class ExerciseHelper {
     }
 
     return ExerciseDetailVO.valueOf(exercise, exerciseTypeRelations);
+  }
+
+  public UserExerciseRoutine createUserExerciseRoutineAndSet(CreateUserExerciseRoutineDTO dto,
+    int totalRoutineSize, UserExerciseLogData userExerciseLogData) {
+    Exercise exercise = exerciseRepository.findById(dto.exerciseId())
+      .orElseThrow(() -> new ResourceException(ResourceExceptionCode.RESOURCE_NOT_FOUND));
+
+    UserExerciseRoutineData routineData = new UserExerciseRoutineData();
+
+    int totalSetSize = dto.setDTOList().size();
+    List<UserExerciseSet> setList = dto.setDTOList().stream()
+      .map(setDTO -> {
+        if (setDTO.serNumber() > totalSetSize) {
+          throw new InvalidInputValueException(InvalidInputValueExceptionCode.INVALID_INPUT_VALUE);
+        }
+
+        UserExerciseSet setEntity = UserExerciseSet.createSet(setDTO);
+        routineData.update(setDTO);
+        return setEntity;
+      }).toList();
+
+    if (dto.order() != null && dto.order() > totalRoutineSize) {
+      throw new InvalidInputValueException(InvalidInputValueExceptionCode.INVALID_INPUT_VALUE);
+    }
+    UserExerciseRoutine routineEntity = UserExerciseRoutine.createRoutine(dto, routineData, exercise);
+
+    setList.forEach(set -> set.applyRoutine(routineEntity));
+
+    userExerciseLogData.setEntityList.addAll(setList);
+    userExerciseLogData.update(routineData);
+    return routineEntity;
   }
 
 }
--- src/main/java/com/example/healthcare/application/exercise/service/UserExerciseLogCmService.java
+++ src/main/java/com/example/healthcare/application/exercise/service/UserExerciseLogCmService.java
@@ -1,12 +1,26 @@
 package com.example.healthcare.application.exercise.service;
 
+import com.example.healthcare.application.account.domain.User;
+import com.example.healthcare.application.account.domain.code.UserStatus;
+import com.example.healthcare.application.account.repository.UserRepository;
+import com.example.healthcare.application.common.exception.ResourceException;
+import com.example.healthcare.application.common.exception.ResourceException.ResourceExceptionCode;
 import com.example.healthcare.application.exercise.controller.dto.CreateUserExerciseLogDTO;
+import com.example.healthcare.application.exercise.controller.dto.CreateUserExerciseRoutineDTO;
+import com.example.healthcare.application.exercise.controller.dto.CreateUserExerciseSetDTO;
 import com.example.healthcare.application.exercise.controller.dto.UpdateUserExerciseLogDTO;
+import com.example.healthcare.application.exercise.domain.Exercise;
+import com.example.healthcare.application.exercise.domain.UserExerciseLog;
+import com.example.healthcare.application.exercise.domain.UserExerciseRoutine;
+import com.example.healthcare.application.exercise.domain.UserExerciseSet;
+import com.example.healthcare.application.exercise.helper.ExerciseHelper;
+import com.example.healthcare.application.exercise.repository.ExerciseRepository;
 import com.example.healthcare.application.exercise.repository.UserExerciseLogRepository;
 import com.example.healthcare.application.exercise.repository.UserExerciseRoutineRepository;
 import com.example.healthcare.application.exercise.repository.UserExerciseSetRepository;
-import com.example.healthcare.application.vo.UserExerciseLogSummaryVO;
+import com.example.healthcare.application.exercise.service.data.UserExerciseData.UserExerciseLogData;
 import com.example.healthcare.application.vo.UserExerciseLogDetailVO;
+import com.example.healthcare.application.vo.UserExerciseLogSummaryVO;
 import com.example.healthcare.application.vo.UserExerciseLogVO;
 import com.example.healthcare.infra.config.security.user.LoginUser;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +28,49 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserExerciseLogCmService {
 
+  private final ExerciseHelper exerciseHelper;
+  private final UserRepository userRepository;
+  private final ExerciseRepository exerciseRepository;
   private final UserExerciseLogRepository userExerciseLogRepository;
   private final UserExerciseRoutineRepository userExerciseRoutineRepository;
   private final UserExerciseSetRepository userExerciseSetRepository;
 
   @Transactional
   public void createExerciseLog(LoginUser loginUser, CreateUserExerciseLogDTO dto) {
+    User user = userRepository.findByIdAndUserStatusIs(loginUser.getId(), UserStatus.ACTIVATED)
+      .orElseThrow(() -> new ResourceException(ResourceExceptionCode.RESOURCE_NOT_FOUND));
 
+    Integer exerciseCount = 0;
+    BigInteger totalSetCount = null;
+    BigInteger totalWeight = null;
+    BigInteger totalReps = null;
+    BigInteger totalTime = null;
+
+    // 운동 종록(routine) + 운동 세트(set)
+    UserExerciseLogData logData = new UserExerciseLogData();
+
+    int routineSize = dto.routineDTOList().size();
+    List<UserExerciseRoutine> routineEntityList = dto.routineDTOList()
+      .stream()
+      .map(routineDTO -> exerciseHelper.createUserExerciseRoutineAndSet(routineDTO, routineSize, logData))
+      .toList();
+
+    // 운동 기록(log)
+    UserExerciseLog logEntity = UserExerciseLog.createLog(dto, user, logData);
+    userExerciseLogRepository.save(logEntity);
+
+    routineEntityList.forEach(routine -> routine.applyLog(logEntity));
+    userExerciseRoutineRepository.saveAll(routineEntityList);
+    userExerciseSetRepository.saveAll(logData.setEntityList);
   }
 
   // Paging 28 ~ 31
