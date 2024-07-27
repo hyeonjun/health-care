fc4ba59f8f447c4965cd04cb6291bf90f86cfd4c
hyeonjun <zlel175@naver.com>
2024-07-28 01:14:07 +0900

    set number 최소 1 이상

--- src/main/java/com/example/healthcare/application/exercise/controller/dto/CreateUserExerciseSetDTO.java
+++ src/main/java/com/example/healthcare/application/exercise/controller/dto/CreateUserExerciseSetDTO.java
@@ -3,10 +3,11 @@ package com.example.healthcare.application.exercise.controller.dto;
 import com.example.healthcare.application.common.exception.InvalidInputValueException;
 import com.example.healthcare.application.common.exception.InvalidInputValueException.InvalidInputValueExceptionCode;
 import com.example.healthcare.application.exercise.domain.code.ExerciseSetType;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateUserExerciseSetDTO(
-  @NotNull Long serNumber,
+  @NotNull @Min(1) Long serNumber,
   @NotNull ExerciseSetType exerciseSetType,
   Long weight,
   Integer reps,
