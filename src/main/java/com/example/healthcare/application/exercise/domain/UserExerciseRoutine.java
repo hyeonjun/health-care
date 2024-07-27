8fbc91099afc17a9ce0c46f45984a0bc58cec21b
hyeonjun <zlel175@naver.com>
2024-07-27 14:52:08 +0900

    volume 필드 추가

--- src/main/java/com/example/healthcare/application/exercise/domain/UserExerciseRoutine.java
+++ src/main/java/com/example/healthcare/application/exercise/domain/UserExerciseRoutine.java
@@ -6,7 +6,11 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Objects;
 
 @Entity
 @DynamicUpdate
@@ -20,6 +24,7 @@ public class UserExerciseRoutine extends Base {
   private Long id;
 
   private Long restTime;
+  private Integer volume;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_exercise_log_id", nullable = false, referencedColumnName = "user_exercise_log_id")
@@ -29,4 +34,29 @@ public class UserExerciseRoutine extends Base {
   @JoinColumn(name = "exercise_id", nullable = false, referencedColumnName = "exercise_id")
   @JsonBackReference
   private Exercise exercise;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof UserExerciseRoutine that)) return false;
+    return Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getId());
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+      .append("id", id)
+      .append("restTime", restTime)
+      .append("volume", volume)
+      .append("userExerciseLog", userExerciseLog)
+      .append("exercise", exercise)
+      .append("createdDateTime", createdDateTime)
+      .append("updatedDateTime", updatedDateTime)
+      .toString();
+  }
 }
